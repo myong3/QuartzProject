f4dbd02c2140ad17022dd9379f555b1ef56ebf81
Owen Huang <myong3333@outlook.com>
2021-09-03 15:01:15 +0800

    加入專案檔案。

---
+++ WebApplication4/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Job;
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            //向DI容器註冊Quartz服務
+            services.AddSingleton<IJobFactory, JobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+
+            //向DI容器註冊Job
+            services.AddSingleton<TestJob>();
+
+            //向DI容器註冊JobSchedule
+            services.AddSingleton(new JobSchedule(jobName: "111", jobType: typeof(TestJob), cronExpression: "0/30 * * * * ?"));
+            services.AddSingleton(new JobSchedule(jobName: "222", jobType: typeof(TestJob), cronExpression: "0/52 * * * * ?"));
+
+            //向DI容器註冊Host服務
+            services.AddSingleton<QuartzHostedService>();
+            services.AddHostedService(provider => provider.GetService<QuartzHostedService>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
